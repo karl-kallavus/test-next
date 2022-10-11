0249b35f49d2d94de8e447b010107fb9129006ac
karl-kallavus <karl@parity.io>
2022-10-11 23:17:11 +0800

    fix

--- next.config.js
+++ next.config.js
@@ -2,6 +2,17 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  experimental: {
+    esmExternals: false,
+  },
+  webpack(config) {
+    if (config.experiments) {
+      config.experiments.topLevelAwait = true;
+    } else {
+      config.experiments = { topLevelAwait: true };
+    }
+    return config;
+  },
+};
 
 module.exports = nextConfig
